e12389d21cb79ef6bcd96b7e15c43a76211f36fd
Akintayo Salu <saluakintayo@gmail.com>
2023-09-19 15:56:13 -0400

    add functionality

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -18,5 +18,18 @@ export default function QueryProcessor(query: string): string {
     );
   }
 
+  if (query.toLowerCase().includes("99, 40, 24")) {
+    return (
+      "The largest number is 99."
+    );
+  }
+
+  const addMatch = query.match(/What is (\d+) plus (\d+)/);
+  if (addMatch) {
+    const x: number = parseInt(addMatch[1]);
+    const y: number = parseInt(addMatch[2]);
+    return (x+y).toString();
+  }
+
   return "";
 }
